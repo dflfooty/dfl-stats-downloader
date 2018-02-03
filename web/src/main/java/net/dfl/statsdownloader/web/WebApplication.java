262551953786d663ad02f6d4e436d514ad7ce264
Keith Lockwood <kwlockwo@gmail.com>
2018-02-03 12:57:57 +1100

    Initial commit

---
+++ web/src/main/java/net/dfl/statsdownloader/web/WebApplication.java
@@ -0,0 +1,13 @@
+package net.dfl.statsdownloader.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class WebApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
