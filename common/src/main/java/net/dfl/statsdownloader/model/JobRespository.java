5c04265cbe9776df1c9cb1cf9ee992976cc4ff46
Keith Lockwood <kwlockwo@gmail.com>
2018-03-10 13:38:27 +1100

    Remove cancelled jobs from cleanup.

--- common/src/main/java/net/dfl/statsdownloader/model/JobRespository.java
+++ common/src/main/java/net/dfl/statsdownloader/model/JobRespository.java
@@ -12,7 +12,7 @@ public interface JobRespository extends JpaRepository<Job, Long> {
 	@Query("select case when count(j) > 0 then true else false end from Job j where j.year = :#{#job.year} and j.round = :#{#job.round} and j.status = 'Submitted'")
 	boolean submittedJobExists(@Param("job") Job job);
 	
-	@Query(value = "select * from job j where j.status not in ('Completed','Failed') and j.updated_at < (now() - interval '10 minutes')",
+	@Query(value = "select * from job j where j.status not in ('Completed','Failed', 'Cancelled') and j.updated_at < (now() - interval '10 minutes')",
 		   nativeQuery = true)
 	List<Job> findUncompletedJobs();
 }
